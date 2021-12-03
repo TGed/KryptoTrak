a36c2cc3ae02fe8ff66d3b15f6199e9bdad7a3e3
TGed <tomekx93@gmail.com>
2021-12-04 00:42:35 +0100

    Przygotowanie

---
+++ app/components/ListItem.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import {Image, View, StyleSheet, TouchableHighlight, ImageComponent } from 'react-native';
+import { Swipeable } from 'react-native-gesture-handler';
+
+import AppText from './AppText';
+import colors from '../config/colors';
+import { render } from 'react-dom';
+
+function ListItem({
+            title, 
+            subTitle, 
+            image, 
+            ImageComponent : IconComponent, 
+            onPress, 
+            renderRightActions,
+        }) {
+    return (
+        <Swipeable renderRightActions={renderRightActions}>
+            <TouchableHighlight 
+            underlayColor={colors.light} 
+            onPress={onPress}
+            >
+                <View style={styles.container}>
+                    {IconComponent}
+                    {image && <Image style={styles.image} source={image}/>}
+                    <View style={styles.detailsContainer}>
+                        <AppText style={styles.title}>{title}</AppText>
+                        {subTitle && <AppText style={styles.subTitle}>{subTitle}</AppText>}
+                    </View>
+                </View>
+            </TouchableHighlight>
+        </Swipeable>
+    );
+}
+
+const styles = StyleSheet.create({
+    container:{
+        flexDirection: "row",
+        padding: 15,
+        backgroundColor: colors.white
+    },
+    detailsContainer:{
+        marginLeft: 10,
+        justifyContent: 'center',
+    },
+    image:{
+        width:70,
+        height: 70,
+        borderRadius: 35,
+    },
+    subTitle:{
+        color: colors.medium,
+    },
+    title:{
+        fontWeight: "500",
+    }
+})
+
+export default ListItem;
